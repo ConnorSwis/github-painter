3e8bb89eecebfd120def38ae07f8530095e3fd1f
ConnorSwis <condude13@gmail.com>
2022-08-22 17:17:23 -0400

    commit

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
 import git
 
-def commit_push():
+
+def commit_push(commit_message: str="commit"):
+    "{bruh} Run {run_number} of {days_runs}"
     try:
         repo = git.Repo('./.git')
         repo.git.execute('git add *')
-        repo.git.execute('git commit -a -m "commit"')
+        repo.git.execute(f'git commit -a -m {commit_message}')
         repo.git.execute('git push')
         return True
     except:
         pass
 
-colors = {
-    0 : (27, 31, 35),
-    1 : (14, 68, 41),
-    2 : (38, 166, 65),
-    3 : (0, 109, 50),
-    4 : (57, 211, 83)
-}
 
 0x1b1f23
 
