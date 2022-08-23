faee3e9a6e5beffc9ccec3d98fd8c04e43ba3caf
ConnorSwis <condude13@gmail.com>
2022-08-23 12:58:05 -0400

    Commit 1/4

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import itertools
 
 logging.basicConfig(
     filename='github_painter.log',
-    format='%(asctime)s:%(levelname)s: %(message)s',
+    format='%(asctime)s:%(levelname)s:%(message)s',
     filemode='w',
     level=logging.INFO
 )
@@ -18,18 +18,15 @@ data = itertools.cycle((0,1,2,3,4)[::-1])
 def run():
     cnt = next(data)
     logger.info(str(cnt))
+    logger.info('')
     for i in range(cnt):
         msg = f'Commit {i+1}/{cnt}'
         commit_push(msg)
 
-t = lambda: threading.Thread(target=run).start()# if start else ...
+t = lambda: threading.Thread(target=run).start()
 
-# def begin():
-#     global start
-#     start = True
 
-# schedule.every().sunday.do(begin)
-schedule.every().day.at("12:52").do(t)
+schedule.every().day.at("12:58").do(t)
 
 while 1:
     schedule.run_pending()
