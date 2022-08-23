d2b64b83d414d8fb608a550e92802ad0c4c08ff1
ConnorSwis <condude13@gmail.com>
2022-08-23 12:02:33 -0400

    commit

--- src/main.py
+++ src/main.py
@@ -17,12 +17,15 @@ data = read_image(fp)
 
 def run():
     cnt = data.pop(0)
+    logger.info(str(cnt))
     for i in range(cnt):
-        commit_push(f'Day {364-len(data)}/364 Commit {i}/{cnt}')
+        gil_dumb = 364-len(data)
+        msg = f'Day {gil_dumb}/364 Commit {i}/{cnt}'
+        commit_push(msg)
 
 t = lambda: threading.Thread(target=run).start()
 
-schedule.every().day.at("00:45").do(t)
+schedule.every().day.at("12:02").do(t)
 
 while 1:
     schedule.run_pending()
